942e7148053863584c6b78db75c38b0de09d5459
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-01-16 10:47:10 +0530

    Langing page updated

--- Components/About/About.tsx
+++ Components/About/About.tsx
@@ -6,19 +6,24 @@ import { HiCheckBadge } from "react-icons/hi2";
 
 const About: React.FC = () => {
   return (
-    <div className='about-container mx-60 p-[20px] rounded-lg'id='about'>
+    <div className='about-container mx-60 p-[20px] rounded-lg' id='about'>
       <div className='bg-white rounded-md py-16'>
         <div><h1 className='about-heading font-semibold'>About Us</h1></div>
         <div className='about-content-container gap-20'>
           <div className="about-content ">
-            <p>We are a platform dedicated to <b>connecting farmers</b> directly with <b>consumers, eliminating</b> middlemen like <b>wholesalers</b> and <b>retailers.</b> Our mission is to support farmers by offering them <b>fair prices</b> for their produce while providing <b>fresh, high-quality</b> agricultural products to consumers.</p>
+            <p>Our platform <b>bridges the gap between farmers and consumers.</b> by <b>eliminating middlemen</b> like <b>wholesalers</b> and <b>retailers.</b> Our mission is to support farmers by offering them <b>fair prices</b> for their produce while providing <b>fresh, high-quality</b> agricultural products to consumers.</p>
+
+            {/* <div>
+              <p><strong>Join us in building a better future for farmers and consumers alike!</strong></p>
+            </div> */}
+
             <div>
               <p>Why You Should Use Our Platform</p>
               <div>
                 <div>
                   <span>
                     <i><HiCheckBadge /></i>
-                    <p> Get fresh agricultural products at competitive prices, with no middlemen involved.</p>
+                    <p> Get fresh agricultural products at competitive prices, with out middlemen involved.</p>
                   </span>
                   <span>
                     <i><HiCheckBadge /></i>

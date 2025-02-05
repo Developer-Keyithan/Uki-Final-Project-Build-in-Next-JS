0674e73a38eb523829fc0fcd8dd8326c36bba3e0
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-02-05 12:51:32 +0530

    Order page completed

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BsCartX } from 'react-icons/bs';
 import Footer from '../../../Components/Footer/Footer';
+import { RiUnpinFill } from 'react-icons/ri';
 
 interface CartItem {
     price: { newPrice: number };
@@ -105,6 +106,14 @@ function CartPage() {
         });
     };
 
+    const handleUnpin = (_id: string) => {
+        setSelectedItems((prevSelectedItems) => {
+            const newSelectedItems = new Set(prevSelectedItems);
+            newSelectedItems.delete(_id); // Remove the item from selected items
+            return newSelectedItems;
+        });
+    };
+
     const calculateProductSubtotal = (item: CartItem) => {
         const itemValue = item.quantity.unit === 'kg'
             ? item.quantity.value  // If the unit is kg, use the value directly
@@ -128,12 +137,12 @@ function CartPage() {
 
     return (
         <div>
-            <div className='sticky top-0'>
+            <div className='sticky top-0 z-50'>
                 <Navbar />
                 <hr />
             </div>
             <div className='flex mx-60 my-10 gap-5'>
-                <div className='flex flex-wrap gap-5 w-3/5 ring-1 ring-gray-300 p-4 rounded-md'>
+                <div className='flex flex-wrap gap-5 w-3/5 ring-1 ring-gray-300 p-4 rounded-md h-[84vh] overflow-y-auto no-scrollbar'>
                     {cartItems.length === 0 ? (
                         <p>Your cart is empty.</p>
                     ) : (
@@ -170,8 +179,8 @@ function CartPage() {
                                             <div className='flex gap-4 my-2'>
                                                 <div className='gap-2'>
                                                     <button
-                                                    className='ring-1 ring-gray-300 px-2 py-[2px] rounded hover:ring-primaryColor hover:bg-primaryColor hover:text-white transition ease-in-out duration-500 cursor-pointer'
-                                                    onClick={() => handleQuantityChange(cartItem._id, cartItem.quantity.value - (cartItem.quantity.unit === 'kg' ? 1 : 50))}>-</button>
+                                                        className='ring-1 ring-gray-300 px-2 py-[2px] rounded hover:ring-primaryColor hover:bg-primaryColor hover:text-white transition ease-in-out duration-500 cursor-pointer'
+                                                        onClick={() => handleQuantityChange(cartItem._id, cartItem.quantity.value - (cartItem.quantity.unit === 'kg' ? 1 : 50))}>-</button>
                                                     <input
                                                         type="number"
                                                         value={cartItem.quantity.value}
@@ -182,8 +191,8 @@ function CartPage() {
                                                         min={1}
                                                     />
                                                     <button
-                                                    className='ring-1 ring-gray-300 px-2 py-[2px] rounded hover:ring-primaryColor hover:bg-primaryColor hover:text-white transition ease-in-out duration-500 cursor-pointer'
-                                                    onClick={() => handleQuantityChange(cartItem._id, cartItem.quantity.value + (cartItem.quantity.unit === 'kg' ? 1 : 50))}>+</button>
+                                                        className='ring-1 ring-gray-300 px-2 py-[2px] rounded hover:ring-primaryColor hover:bg-primaryColor hover:text-white transition ease-in-out duration-500 cursor-pointer'
+                                                        onClick={() => handleQuantityChange(cartItem._id, cartItem.quantity.value + (cartItem.quantity.unit === 'kg' ? 1 : 50))}>+</button>
                                                 </div>
                                                 <label htmlFor={`kg-${cartItem._id}`} className='flex items-center gap-2'>
                                                     <input
@@ -223,8 +232,59 @@ function CartPage() {
                         ))
                     )}
                 </div>
-                <div className="w-2/5">
-                    <p>Total: {calculateCartTotal()} LKR</p>
+                <div className="relative w-2/5 h-[84vh] z-0">
+                    <h2 className='font-semibold text-2xl mb-5'>Order Summary</h2>
+                    <div className="h-[80%] overflow-y-auto no-scrollbar">
+                        {selectedItems.size === 0 ? (
+                            <p className="flex items-center justify-center text-gray-500 py-4 w-full h-full">You didn't select an item for order</p>
+                        ) : (
+                            [...selectedItems].map((selectedItemId) => {
+                                const selectedItem = cartItems.find(item => item._id === selectedItemId);
+                                return selectedItem ? (
+                                    <div key={selectedItem._id} className='flex p-4 border-b-[1px] hover:bg-gray-100 transition ease-in-out duration-300'>
+                                        <div className='w-1/3 h-full'>
+                                            {/* <Image
+                                                src={selectedItem.productImages[0].imageUrl}
+                                                alt={selectedItem.productName}
+                                                width={100}
+                                                height={100}
+                                                objectFit="cover"
+                                                className="h-full w-full object-cover"
+                                            /> */}
+                                        </div>
+                                        <div className='w-2/3'>
+                                            <div className='flex justify-between items-start w-full'>
+                                                <h3 className='font-semibold'>{selectedItem.productName}</h3>
+                                                <p className='w-max px-4 py-1 bg-gray-200 text-center rounded'>
+                                                    <span className='font-semibold'>Sub Total: </span>{calculateProductSubtotal(selectedItem)} LKR
+                                                </p>
+                                            </div>
+                                            <div className='flex justify-between items-center'>
+                                                <div className='mt-2'>
+                                                    <p><strong className='font-semibold'>Qty: </strong>{selectedItem.quantity.value} {selectedItem.quantity.unit}</p>
+                                                    <p><span className='font-semibold'>Price Per kg: </span>{selectedItem.price.newPrice} LKR</p>
+                                                </div>
+                                                <button
+                                                    onClick={() => handleUnpin(selectedItem._id)}
+                                                    className='text-red-700 text-xl h-max w-max p-1 rounded-2xl hover:bg-red-700 hover:text-white transition ease-in-out duration-500'
+                                                    aria-label={`Remove ${selectedItem.productName} from Order Summary`}
+                                                >
+                                                    <RiUnpinFill />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : null;
+                            })
+                        )}
+                    </div>
+                    <div className="absolute bottom-0 w-full">
+                        <div className='flex justify-between items-center py-4'>
+                            <p className='font-semibold'>{selectedItems.size} Items Selected</p>
+                            <p className='font-semibold text-xl'>Total: {calculateCartTotal()} LKR</p>
+                        </div>
+                        <button className='bg-primaryColor text-white w-full px-8 py-2 rounded'>Checkout</button>
+                    </div>
                 </div>
             </div>
             <Footer />

de9933244c541adef69b29e40ecce22abcb71b13
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-02-21 13:02:36 +0530

    Error fixed for deplyment1

--- fermire/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['fermier-project.s3.ap-south-1.amazonaws.com'],
-  },
-};
-
-export default nextConfig;
